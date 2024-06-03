ea286dea45b5d8794a0476161fe4a66c0dad1942
Shivansh Rawat <shivanshrawat587@gmail.com>
2024-06-03 12:41:02 +0530

    updated schema

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -13,6 +13,7 @@ export const GET = async () => {
 
     return new NextResponse(JSON.stringify(posts, { status: 200 }));
   } catch (error) {
+    console.log(error);
     return new NextResponse(
       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
     );
